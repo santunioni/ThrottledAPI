870be0debc4221812e201c668b0f4eab67823e47
Vinícius Vargas <vinicius.svargas@luizalabs.com>
2022-01-22 18:04:12 -0300

    fix: import time in models

--- throttled/fastapi/handler.py
+++ throttled/fastapi/handler.py
@@ -1,18 +1,11 @@
-from typing import Callable, Tuple, Type
-
 from starlette import status
 from starlette.requests import Request
 from starlette.responses import Response
 
 from throttled.exceptions import RateLimitExceeded
 
-ExceptionType = Type[Exception]
-HandlerType = Callable[[Request, Exception], Response]
 
-FastAPIHandler = Tuple[ExceptionType, HandlerType]
-
-
-def handle_rate_exceed_func(request: Request, exc: RateLimitExceeded) -> Response:
+def handle_rate_exceed_func(_: Request, exc: RateLimitExceeded) -> Response:
     return Response(
         status_code=status.HTTP_429_TOO_MANY_REQUESTS,
         content=f"Rate exceeded for key={exc.key}.",
@@ -20,4 +13,4 @@ def handle_rate_exceed_func(request: Request, exc: RateLimitExceeded) -> Respons
     )
 
 
-handle_rate_exceed: FastAPIHandler = (RateLimitExceeded, handle_rate_exceed_func)
+handle_rate_exceed = (RateLimitExceeded, handle_rate_exceed_func)
