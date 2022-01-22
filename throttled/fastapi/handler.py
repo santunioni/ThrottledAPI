cdf02ab07f3a24c6af61cd48f655f88547b9b089
Vinícius Vargas <vinicius.svargas@luizalabs.com>
2022-01-22 10:26:46 -0300

    feat: add retry-after to handler

--- throttled/fastapi/handler.py
+++ throttled/fastapi/handler.py
@@ -16,6 +16,7 @@ def handle_rate_exceed_func(request: Request, exc: RateLimitExceeded) -> Respons
     return Response(
         status_code=status.HTTP_429_TOO_MANY_REQUESTS,
         content=f"Rate exceeded for key={exc.key}.",
+        headers={"Retry-After": exc.retry_after},
     )
 
 
