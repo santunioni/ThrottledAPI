3afb4f71bb733dba40f4e70a866004bd618ef5f3
Vinícius Vargas <vinicius.svargas@luizalabs.com>
2022-01-22 11:55:29 -0300

    feat: host based limiter

--- throttled/fastapi/limiters.py
+++ throttled/fastapi/limiters.py
@@ -1,11 +1,33 @@
+from abc import ABC, abstractmethod
+from typing import Callable, Sequence
+
+from fastapi import Depends
+from starlette.requests import Request
+
 from throttled import Hit
 from throttled.strategies import Strategy
 
 
-class SimpleLimiter:
+class Limiter(ABC):
     def __init__(self, strategy: Strategy):
-        self.__strategy = strategy
+        self._strategy = strategy
 
+    @abstractmethod
+    def __call__(self, *args, **kwargs):
+        """Limit the api based on rate."""
+
+
+class GlobalLimiter(Limiter):
     def __call__(self):
         hit = Hit()
-        self.__strategy(hit)
+        self._strategy(hit)
+
+
+class HostBasedLimiter(Limiter):
+    def __call__(self, request: Request):
+        hit = Hit(key=request.client.host)
+        self._strategy(hit)
+
+
+def as_dependencies(limiters: Sequence[Callable]) -> Depends:
+    return [Depends(limiter) for limiter in limiters]
