7dcca6d7ee1b453faaf7be6f459b68ce654a1c04
Vinícius Vargas <vinicius.svargas@luizalabs.com>
2022-01-23 18:43:14 -0300

    fix: wrong import

--- throttled/fastapi/__init__.py
+++ throttled/fastapi/__init__.py
@@ -1,4 +1,6 @@
 from .builder import FastAPILimiterBuilder
-from .limiters import HostBasedLimiter, Limiter, TotalLimiter
+from .limiters.base import Limiter
+from .limiters.host import HostBasedLimiter
+from .limiters.total import TotalLimiter
 
 __all__ = ["TotalLimiter", "HostBasedLimiter", "Limiter", "FastAPILimiterBuilder"]
--- throttled/fastapi/limiters/__init__.py
+++
@@ -1,5 +0,0 @@
-from .base import Limiter, Middleware
-from .host import HostBasedLimiter
-from .total import TotalLimiter
-
-__all__ = ["Middleware", "TotalLimiter", "HostBasedLimiter", "Limiter"]
