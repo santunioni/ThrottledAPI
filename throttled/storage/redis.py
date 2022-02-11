cafadf5fcc13ea344083ae6b3cbd5cfa1908d76f
Vinícius Vargas <vinicius.svargas@luizalabs.com>
2022-02-11 20:25:09 -0300

    feat: implement strategies with redis storage

--- throttled/storage/redis.py
+++ throttled/storage/redis.py
@@ -1,48 +1,55 @@
-from typing import Type
-
 from redis.client import Redis
 
 from throttled.models import Hit, Rate
+from throttled.storage._duration_funcs import DURATION_FUNCTIONS, DurationCalcType
 from throttled.storage.abstract import HitsWindow, WindowManager
 from throttled.strategy.base import Storage, Strategy
 
 
 class _RedisWindow(HitsWindow):
-    __slots__ = ("__client", "__duration", "__key")
-
-    def __init__(self, client: Redis, duration: int, key: str):
-        self.__client = client
-        self.__duration = duration
-        self.__key = key
-        super().__init__()
-
-    def incr(self, hits: int = 1) -> int:
-        value = self.__client.incrby(name=self.__key, amount=hits)
-        if value == hits:
-            self.__client.expire(name=self.__key, time=self.__duration)
-        return value
-
-    def decr(self, hits: int = 1) -> int:
-        value = self.__client.decrby(name=self.__key, amount=hits)
-        return value
-
-    def get_remaining_seconds(self) -> float:
-        return self.__client.pttl(name=self.__key) / 1000
-
-
-class _RedisWindowManager(WindowManager):
-    __slots__ = ("__interval", "__client")
+    __slots__ = ("__client", "__interval", "__key", "__duration_calc")
 
     def __init__(
-        self,
-        interval: int,
-        client: Redis,
+        self, client: Redis, interval: int, hit: Hit, duration_func: DurationCalcType
     ):
         self.__client = client
         self.__interval = interval
+        self.__hit = hit
+        self.__duration_func = duration_func
+        super().__init__()
+
+    def incr(self, hits: int = 1) -> int:
+        value = self.__client.incrby(name=self.__hit.key, amount=hits)
+        if value == hits:
+            self.__client.pexpire(
+                name=self.__hit.key,
+                time=int(1000 * self.__duration_func(self.__hit.time, self.__interval)),
+            )
+        return value
+
+    def decr(self, hits: int = 1) -> int:
+        value = self.__client.decrby(name=self.__hit.key, amount=hits)
+        return value
+
+    def get_remaining_seconds(self) -> float:
+        return self.__client.pttl(name=self.__hit.key) / 1000
+
+
+class _RedisWindowManager(WindowManager):
+    __slots__ = ("__interval", "__client", "__duration_calc")
+
+    def __init__(self, interval: int, client: Redis, duration_func: DurationCalcType):
+        self.__client = client
+        self.__interval = interval
+        self.__duration_func = duration_func
 
     def get_current_window(self, hit: Hit) -> _RedisWindow:
-        return _RedisWindow(client=self.__client, duration=self.__interval, key=hit.key)
+        return _RedisWindow(
+            client=self.__client,
+            interval=self.__interval,
+            hit=hit,
+            duration_func=self.__duration_func,
+        )
 
 
 class RedisStorage(Storage):
@@ -53,10 +60,11 @@ class RedisStorage(Storage):
         self.__client = client
 
     def get_window_manager(
-        self, strategy: Type[Strategy], limit: Rate
+        self, strategy: Strategy, limit: Rate
     ) -> _RedisWindowManager:
         return _RedisWindowManager(
             interval=int(limit.interval),
+            duration_func=DURATION_FUNCTIONS[strategy.__class__],
             client=self.__client,
         )
 
