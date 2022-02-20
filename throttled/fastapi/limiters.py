c43c44e39340bc90dd905d5dd5ddb0f46efa018d
Vinícius Vargas <vinicius.svargas@luizalabs.com>
2022-02-20 11:46:59 -0300

    fix: use ip fetching already implemented in Starlette

--- throttled/fastapi/limiters.py
+++ throttled/fastapi/limiters.py
@@ -5,10 +5,7 @@ from .base import MiddlewareLimiter
 
 class IPLimiter(MiddlewareLimiter):
     def __call__(self, request: Request):
-        host = request.headers.get(
-            "X_FORWARDED_FOR", request.client.host or "127.0.0.1"
-        )
-        self.limit(key=f"host={host}")
+        self.limit(key=f"host={request.client.host}")
 
 
 class TotalLimiter(MiddlewareLimiter):
